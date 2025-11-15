deb98e6104388195727738bbc05e629f5c8f4bc2
vicviper <vfernandez8990@gmail.com>
2025-11-15 06:01:28 -0600

    update

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,6 +1,3 @@
-using System;
-using System.IO;
-using System.Net.Http;
 using System.Text;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
@@ -12,38 +9,30 @@ namespace EsepWebhook;
 
 public class Function
 {
-    private static readonly HttpClient client = new HttpClient();
     
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
     public string FunctionHandler(object input, ILambdaContext context)
     {
         context.Logger.LogInformation($"FunctionHandler received: {input}");
 
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
 
-
-        context.Logger.LogInformation($"Body: {json.body}");
-        dynamic body = JsonConvert.DeserializeObject<dynamic>(json.body.ToString());
-
-    
-        string issueUrl = body.issue.html_url;
-        context.Logger.LogInformation($"Issue URL: {issueUrl}");
-
-        string payload = JsonConvert.SerializeObject(new
-        {
-            text = $"Issue Created: {issueUrl}"
-        });
-
-        // 5) Read Slack webhook from env var (NO hard-coding)
-        string slackUrl = Environment.GetEnvironmentVariable("SLACK_URL");
-
-        var request = new HttpRequestMessage(HttpMethod.Post, slackUrl)
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
         {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
         };
-
-        var response = client.Send(request);
-        string responseBody = response.Content.ReadAsStringAsync().Result;
-
-        return responseBody;
+    
+        var response = client.Send(webRequest);
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+            
+        return reader.ReadToEnd();
     }
 }
